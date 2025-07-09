01638b7315f5edf9f75c4608bd079bc94d7b3adf
saeeyes <133561400+saeeyes@users.noreply.github.com>
2025-07-09 13:54:40 +0900

    chore : SwaggerConfig modify for security

--- src/main/java/com/example/dr_aids/config/SwaggerConfig.java
+++ src/main/java/com/example/dr_aids/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.example.dr_aids.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,7 +11,19 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI openAPI() {
+    public OpenAPI customOpenAPI() {
+
+        // Security Scheme 정의
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
+        // Security Requirement 정의
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("BearerAuth");
+
         Info info = new Info()
                 .version("v1.0") //버전
                 .title("Dr-Aids API") //이름
