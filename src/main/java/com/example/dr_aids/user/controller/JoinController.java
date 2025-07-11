eae0ff7412d4ebde2d47de41ef00eb95a2ef8562
saeeyes <133561400+saeeyes@users.noreply.github.com>
2025-07-11 14:23:23 +0900

    feat : join 구현

--- src/main/java/com/example/dr_aids/user/controller/JoinController.java
+++ src/main/java/com/example/dr_aids/user/controller/JoinController.java
@@ -1,5 +1,7 @@
 package com.example.dr_aids.user.controller;
 
+import com.example.dr_aids.exception.CustomException;
+import com.example.dr_aids.exception.ErrorCode;
 import com.example.dr_aids.user.domain.JoinDTO;
 import com.example.dr_aids.user.service.JoinService;
 import lombok.extern.slf4j.Slf4j;
@@ -13,6 +15,20 @@ import java.util.Collections;
 @Slf4j
 @RestController
 public class JoinController {
+    private final JoinService joinService;
 
+    public  JoinController(JoinService joinService){
+        this.joinService = joinService;
+    }
+
+    @PostMapping("/join")
+    public ResponseEntity<?> joinProcess(@RequestBody JoinDTO joinDTO){
+        boolean success = joinService.joinProcess(joinDTO);
+        if (success) {
+            return ResponseEntity.ok(Collections.singletonMap("message", "success"));
+        } else {
+            throw new CustomException(ErrorCode.USER_ALREADY_EXISTS); // 기존 방식 대신 예외 던지기
+        }
+    }
 
 }
--- src/main/java/com/example/dr_aids/user/service/JoinService.java
+++ src/main/java/com/example/dr_aids/user/service/JoinService.java
@@ -1,4 +1,50 @@
 package com.example.dr_aids.user.service;
 
+import com.example.dr_aids.exception.CustomException;
+import com.example.dr_aids.exception.ErrorCode;
+import com.example.dr_aids.user.domain.JoinDTO;
+import com.example.dr_aids.user.domain.Role;
+import com.example.dr_aids.user.domain.User;
+import com.example.dr_aids.user.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+
+@Slf4j
+@Service
+@AllArgsConstructor
 public class JoinService {
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Boolean joinProcess(JoinDTO joinDTO) {
+        String username = joinDTO.getUsername();
+        String password = joinDTO.getPassword();
+        String email = joinDTO.getEmail();
+        String role = joinDTO.getRole();
+
+        if(username == null || password == null || email == null || role == null) {
+            log.info("joinProcess 실패");
+            throw new CustomException(ErrorCode.INVALID_REQUEST);
+
+        }
+        Boolean isExist = userRepository.existsByEmail(email);
+
+        if (isExist) {
+            throw new CustomException(ErrorCode.USER_ALREADY_EXISTS, "이미 존재하는 이메일입니다.");
+        }
+
+        User data = new User();
+
+        data.setUsername(username);
+        data.setPassword(bCryptPasswordEncoder.encode(password));
+        data.setEmail(email);
+        data.setRole(Role.valueOf(role));
+
+        userRepository.save(data);
+        return true;
+    }
 }
+
