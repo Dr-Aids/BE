329e8f1c67ea7e93c0c0f023cb709f45f7223348
saeeyes <133561400+saeeyes@users.noreply.github.com>
2025-07-11 13:15:31 +0900

    feat : BaseEntity 구현

--- src/main/java/com/example/dr_aids/security/domain/RefreshToken.java
+++ src/main/java/com/example/dr_aids/security/domain/RefreshToken.java
@@ -1,5 +1,6 @@
 package com.example.dr_aids.security.domain;
 
+import com.example.dr_aids.util.BaseEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,7 +13,7 @@ import java.util.Date;
 @Entity
 @Getter
 @Setter
-public class RefreshToken {
+public class RefreshToken extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
